0b0065548b8e1a54f2ebdbc6f296c48f29c81d48
Krunal Modi <krunalmodi59@gmail.com>
2025-03-24 17:11:39 +0530

    Add project files.

---
+++ Business/AuthService/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentGateway.Models;
+using System.Collections.Generic;
+
+namespace PaymentGateway.Business.AuthService
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Business/AuthService/Interface/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using PaymentGateway.Models;
+
+namespace PaymentGateway.Business.AuthService.Interface
+{
+    public interface IAuthService
+    {
+        public Task<User> Login(string email, string password);
+        public Task<User> Register(User user);
+    }
+}
